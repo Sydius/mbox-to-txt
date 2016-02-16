ef47382e0a7e70d893381b16967411504e3b6da7
Christopher Ogden <sydius@gmail.com>
2016-02-15 17:19:39 -0800

    Comment methods

--- mbox_to_corpus.py
+++ mbox_to_corpus.py
@@ -3,6 +3,12 @@ import re
 
 
 def munge_message(text):
+    """
+    Munge an e-mail message (in text form).
+
+    :param text: The e-mail message.
+    :return: The munged e-mail message.
+    """
     text = re.sub(r'(\n|^)On.*\n?.*wrote:\n+(.|\n)*$', '', text)
     text = re.sub(r'(\n|^)From:(.|\n)*$', '', text)
     text = re.sub(r'(\n|^)---------- Forwarded message ----------(.|\n)*$', '', text)
@@ -12,6 +18,12 @@ def munge_message(text):
 
 
 def unquoted_line(line):
+    """
+    Unquote an e-mail message line according to RFC 3676.
+
+    :param line: The (possibly quoted) message line.
+    :return: (unquoted line, quote depth).
+    """
     quote_depth = 0
     while line.startswith('>'):
         line = line[1:]
@@ -20,12 +32,25 @@ def unquoted_line(line):
 
 
 def unstuff_line(line):
+    """
+    Unstuff an e-mail message line according to RFC 3637.
+
+    :param line: The (possibly stuffed) message line.
+    :return: The unstuffed message line.
+    """
     if line.startswith(' '):
         return line[1:]
     return line
 
 
 def unflow_line(line, delsp):
+    """
+    Unflow an e-mail message line according to RFC 3637.
+
+    :param line: The (possibly soft-broken) message line.
+    :param delsp: Whether or not soft-break spaces should be deleted.
+    :return: (processed line, soft-broken)
+    """
     if len(line) < 1:
         return (line, False)
     if line.endswith(' '):
@@ -36,6 +61,13 @@ def unflow_line(line, delsp):
 
 
 def unflow_text(text, delsp):
+    """
+    Unflow an e-mail message according to RFC 3637.
+
+    :param text: The flowed message.
+    :param delsp: Whether or not soft-break spaces should be deleted.
+    :return: The processed message.
+    """
     full_line = ''
     full_text = ''
     lines = text.splitlines()
@@ -51,6 +83,14 @@ def unflow_text(text, delsp):
 
 
 def part_to_text(part):
+    """
+    Converts an e-mail message part into text.
+
+    Returns None if the message could not be decoded as ASCII.
+
+    :param part: E-mail message part.
+    :return: Message text.
+    """
     if part.get_content_type() != 'text/plain':
         return None
     charset = part.get_content_charset()
@@ -69,6 +109,14 @@ def part_to_text(part):
 
 
 def message_to_text(message):
+    """
+    Converts an e-mail message into text.
+
+    Returns an empty string if the e-mail message could not be decoded as ASCII.
+
+    :param message: E-mail message.
+    :return: Message text.
+    """
     text = ''
     for part in message.walk():
         part = part_to_text(part)
@@ -78,6 +126,13 @@ def message_to_text(message):
 
 
 def print_mailbox(mailbox, author):
+    """
+    Print the contents of a mailbox to standard-out, excluding messages to 'author' and not from 'author'.
+
+    :param mailbox: Mailbox to print.
+    :param author: Excludes messages to and not from this author.
+    :return: Nothing.
+    """
     for message in mailbox:
         if not message['From']:
             continue
